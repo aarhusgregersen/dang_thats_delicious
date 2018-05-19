2a579159c603c732ad2fbff993e2d6c51921c161
Martin Aarhus Gregersen <aarhus@dinero.dk>
2018-05-19 17:12:54 +0200

    Adds flashes for createStore

--- controllers/storeController.js
+++ controllers/storeController.js
@@ -11,7 +11,7 @@ exports.addStore = (req, res) => {
 }
 
 exports.createStore = async (req, res) => {
-    const store = new Store(req.body);
-    await store.save(); // We are not moving on untill this save has happened
-    res.redirect('/');
+    const store = await (new Store(req.body)).save();
+    req.flash('success', `Successfully created ${store.name}. Care to leave a review?`);
+    res.redirect(`/store/${store.slug}`);
 }
