8ff7d6a06876c83341cec4c0793b82f05cb70aa8
Martin Aarhus Gregersen <aarhus@dinero.dk>
2018-05-23 11:07:30 +0200

    Adds DB query for store, plus view and mixins for store

--- controllers/storeController.js
+++ controllers/storeController.js
@@ -15,3 +15,9 @@ exports.createStore = async (req, res) => {
     req.flash('success', `Successfully created ${store.name}. Care to leave a review?`);
     res.redirect(`/store/${store.slug}`);
 }
+
+exports.getStores = async (req, res) => {
+    // 1. Query database for a list of all stores
+    const stores = await Store.find(); // Returns a promise
+    res.render('stores', {title: 'Stores', stores});
+}
