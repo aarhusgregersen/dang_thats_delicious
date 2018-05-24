85e1620062183d3cdbd50185cd0db183a10d7b25
Martin Aarhus Gregersen <aarhus@dinero.dk>
2018-05-24 15:19:28 +0200

    Updates app to include edit flow. Modifies add-flow to include

--- controllers/storeController.js
+++ controllers/storeController.js
@@ -21,3 +21,24 @@ exports.getStores = async (req, res) => {
     const stores = await Store.find(); // Returns a promise
     res.render('stores', {title: 'Stores', stores});
 }
+
+exports.editStore = async (req, res) => {
+    // 1. find the store given the id
+    const store = await Store.findOne({ _id: req.params.id});
+    // 2. confirm they are the owner of the store
+    // TODO
+    // 3. Render out the edit form
+    res.render('editStore', {title: `Edit ${store.name}`, store});
+}
+
+exports.updateStore = async (req, res) => {
+    // Find and update store
+    const store = await Store.findOneAndUpdate({ _id: req.params.id }, req.body, {
+        new: true, // Return new store instead of the old one
+        runValidators: true, // Force model to run required validators
+    }).exec(); // Forces query to run
+    // Tell them it worked (flash)
+    req.flash("success", `Sucessfully updated <strong>${store.name}</strong>. <a href="/stores/${store.slug}">View Store -></a>`);
+    // Redirect to store
+    res.redirect(`/stores/${store.id}/edit`);
+}
