b0466e3a5e02a98d0b9dcf6e032289c0fcc82954
Martin Aarhus Gregersen <aarhus@dinero.dk>
2018-05-29 16:12:22 +0200

    Adds image and upload support

--- controllers/storeController.js
+++ controllers/storeController.js
@@ -1,5 +1,20 @@
 const mongoose = require('mongoose');
 const Store = mongoose.model('Store');
+const multer = require('multer');
+const jimp = require('jimp');
+const uuid = require('uuid');
+
+const multerOptions = {
+    storage: multer.memoryStorage(),
+    fileFilter(req, file, next) { //ES6 function
+        const isPhoto = file.mimetype.startsWith('image/');
+        if(isPhoto) {
+            next(null, true);
+        } else {
+            next({message: 'That filetype isn\'t allowed!'}, false);
+        }
+    }
+}
 
 exports.homePage = (req, res) => {
     console.log(req.name);
@@ -8,6 +23,23 @@ exports.homePage = (req, res) => {
 
 exports.addStore = (req, res) => {
     res.render('editStore', {title: 'Add Store'});
+}
+
+exports.upload = multer(multerOptions).single('photo');
+
+exports.resize = async (req, res, next) => {
+    // Check if there is no new file to resize
+    if (!req.file) {
+        next(); // Skip to next middleware
+        return;
+    }
+    const extension = req.file.mimetype.split('/')[1];
+    req.body.photo = `${uuid.v4()}.${extension}`; // Saves file to use for next requests
+    const photo = await jimp.read(req.file.buffer); // Save file to const, when buffer is ready
+    await photo.resize(800, jimp.AUTO); // Resize element
+    await photo.write(`./public/uploads/${req.body.photo}`);
+    // Once we have written the photo to our system, keep going!
+    next();
 }
 
 exports.createStore = async (req, res) => {
